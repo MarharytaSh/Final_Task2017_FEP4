e881b9d7f7a251da05b4f9e4bca353400f8057a7
Margarita <margarita.sharkan.modera@gmail.com>
2017-11-23 17:43:14 +0200

    added function to obj task

--- serverForMapHtml.js
+++ serverForMapHtml.js
@@ -5,22 +5,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
-
 var fs = require("fs");
-
-app.get('/', (request, response) => {
-        console.log('GET');
-        var form = fs.readFileSync('public/mapHtml.html');
-        response.send(form.toString());
-    }
-);
-
-app.get('/allTasks', (request, response)=>{
-    console.log('GET allTasks');
-    var read_task_json = fs.readFileSync('public/tasks_json.json', 'utf-8');
-    response.send(read_task_json);
-});
-
 
 function makeID(arr){
     var max = 0;
@@ -30,22 +15,6 @@ function makeID(arr){
     console.log(max);
     return max + 1;
 }
-
-app.post('/TASK', (request, response) => {
-        console.log('POST');
-        console.log(request.body);
-        var serverJSONwithTasks = fs.readFileSync('public/tasks_json.json', 'utf-8');
-        var arr = JSON.parse(serverJSONwithTasks);
-        var count = makeID(arr);
-        request.body.ServiceType.id = JSON.stringify(count);
-        arr.push(request.body);
-        var arr = JSON.stringify(arr);
-         fs.writeFileSync('public/tasks_json.json', arr, 'utf-8');
-         var json = fs.readFileSync('public/tasks_json.json', 'utf-8');
-        response.send(json);
-    }
-);
-
 function findIdANDDelete (arr, id){
     var Notmatches = [];
     for(var x=0; x<arr.length; x++){
@@ -55,22 +24,6 @@ function findIdANDDelete (arr, id){
     }
     return  Notmatches;
 }
-
-app.delete('/deleteUser', (request, response)=> {
-        console.log('DELETE');
-        var id = request.body.id;
-
-        var serverJSONwithTasks = fs.readFileSync('public/tasks_json.json', 'utf-8');
-        var arr = JSON.parse(serverJSONwithTasks);
-        var arr = findIdANDDelete (arr, id);
-        var arr = JSON.stringify(arr);
-        fs.writeFileSync('public/tasks_json.json', arr, 'utf-8');
-        var json = fs.readFileSync('public/tasks_json.json', 'utf-8');
-        var json = JSON.parse(json);
-        console.log('deleted');
-        response.send(json);
-    }
-);
 function changeDate(arr, id){
     var match = {};
     for(var x=0; x<arr.length; x++){
@@ -82,29 +35,58 @@ function changeDate(arr, id){
     return  match;
 }
 
-app.put('/editeUserONPage', (request, response)=> {
-        console.log('PUT');
-        var id = request.body.id;
 
-        var serverJSONwithTasks = fs.readFileSync('public/tasks_json.json', 'utf-8');
-        var arr = JSON.parse(serverJSONwithTasks);
-        var match = changeDate (arr, id);
-        var match = JSON.stringify(match);
-
-        response.send(match);
+app.get('/', (request, response) => {
+    console.log('GET');
+    var form = fs.readFileSync('public/mapHtml.html');
+    response.send(form.toString());
     }
 );
-
-
-function findIdANDDelete (arr, id){
-    var Notmatches = [];
-    for(var x=0; x<arr.length; x++){
-        if(arr[x]['ServiceType'].id !== id){
-            Notmatches.push(arr[x]);
-        }
-    }
-    return  Notmatches;
+app.get('/allTasks', (request, response)=>{
+    console.log('GET allTasks');
+    var read_task_json = fs.readFileSync('public/tasks_json.json', 'utf-8');
+    response.send(read_task_json);
+});
+app.post('/TASK', (request, response) => {
+    console.log('POST');
+    console.log(request.body);
+    var serverJSONwithTasks = fs.readFileSync('public/tasks_json.json', 'utf-8');
+    var arr = JSON.parse(serverJSONwithTasks);
+    var count = makeID(arr);
+    request.body.ServiceType.id = JSON.stringify(count);
+    arr.push(request.body);
+    var arr = JSON.stringify(arr);
+     fs.writeFileSync('public/tasks_json.json', arr, 'utf-8');
+     var json = fs.readFileSync('public/tasks_json.json', 'utf-8');
+    response.send(json);
 }
+);
+app.delete('/deleteUser', (request, response)=> {
+    console.log('DELETE');
+    var id = request.body.id;
+
+    var serverJSONwithTasks = fs.readFileSync('public/tasks_json.json', 'utf-8');
+    var arr = JSON.parse(serverJSONwithTasks);
+    var arr = findIdANDDelete (arr, id);
+    var arr = JSON.stringify(arr);
+    fs.writeFileSync('public/tasks_json.json', arr, 'utf-8');
+    var json = fs.readFileSync('public/tasks_json.json', 'utf-8');
+    var json = JSON.parse(json);
+    console.log('deleted');
+    response.send(json);
+}
+);
+app.put('/editeUserONPage', (request, response)=> {
+    console.log('PUT');
+    var id = request.body.id;
+
+    var serverJSONwithTasks = fs.readFileSync('public/tasks_json.json', 'utf-8');
+    var arr = JSON.parse(serverJSONwithTasks);
+    var match = changeDate (arr, id);
+    var match = JSON.stringify(match);
+    response.send(match);
+}
+);
 
 app.put('/TASKchangedTOsave', (request, response)=> {
     console.log('PUT TASK_changed_TO_save');
@@ -121,8 +103,6 @@ app.put('/TASKchangedTOsave', (request, response)=> {
         response.send(jsonREAD);
     }
 );
-
-
 
 
 app.listen(3006, (request, response) => {
